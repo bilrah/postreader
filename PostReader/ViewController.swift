e5626cf8e753223a5fe8c098c78ad6b1a96dce73
Bilal A Rahman <bilalarahman@factweavers.com>
2021-09-05 14:42:24 +0530

    Initial Commit

---
+++ PostReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PostReader
+//
+//  Created by Bilal Rahman on 05/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
