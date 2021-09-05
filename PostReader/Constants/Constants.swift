4155fb18d635df07ddad5e958aef2ee1d4af1fbb
Bilal Rahman <bilalrahman@Bilals-MacBook-Pro.local>
2021-09-05 16:57:03 +0530

    constants and renaming main screen and viewcontroller

---
+++ PostReader/Constants/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  PostReader
+//
+//  Created by Bilal Rahman on 05/09/21.
+//
+
+import Foundation
+
+
+struct Constants {
+    struct APIConstants {
+        static let FETCH_POSTS_API = "https://gorest.co.in/public-api/posts"
+    }
+}
