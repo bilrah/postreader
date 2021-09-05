84dd8ab41d2f71e9456cb524dd0e9890d7433bc7
Bilal Rahman <bilalrahman@Bilals-MacBook-Pro.local>
2021-09-05 17:38:51 +0530

    adding git ignore and initializing posts content table view cell

--- PostReader/Screens/PostsScreen/PostsViewController.swift
+++ PostReader/Screens/PostsScreen/PostsViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class PostsViewController: UIViewController {
-
+    
+    @IBOutlet weak var postsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        registerTableViewCells()
+        postsTableView.delegate = self
+        postsTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
-
-
+    
+    private func registerTableViewCells() {
+        postsTableView.register(
+            UINib(
+                nibName: "PostContentTableViewCell",
+                bundle: nil),
+            forCellReuseIdentifier: "PostContentTVC")
+    }
 }
 
+extension PostsViewController: UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tableCell =
+            tableView.dequeueReusableCell(
+                withIdentifier:
+                    "PostContentTVC",
+                for: indexPath) as?
+            PostContentTableViewCell
+        return tableCell!
+    }
+    
+    
+}
---
+++ PostReader/Views/PostScreenViews/PostContentTableVIewCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostContentTableVIewCellTableViewCell.swift
+//  PostReader
+//
+//  Created by Bilal Rahman on 05/09/21.
+//
+
+import UIKit
+
+class PostContentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
